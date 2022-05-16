621a2d16cfa3ffa6a7128320a2c30048f93fe12a
chaitraliawasare <92008200+chaitraliawasare@users.noreply.github.com>
2022-05-16 10:18:58 +0000

    Update selenium.java

--- selenium.java
+++ selenium.java
@@ -13,27 +13,27 @@ public class selenium {
      driver.navigate().to("http://172.168.1.150:8080/login");
      Thread.sleep(3000);
      driver.findElement(By.xpath("/html/body/div/div/div/div/span/a")).click();
-     driver.findElement(By.id("firstName")).sendKeys("Smith");
+     driver.findElement(By.id("firstName")).sendKeys("Tom");
      driver.findElement(By.id("lastName")).sendKeys("Williams");
-     driver.findElement(By.id("email")).sendKeys("Smith@tcs.com");
-     driver.findElement(By.id("password")).sendKeys("1999");
+     driver.findElement(By.id("email")).sendKeys(Tom@tcs.com");
+     driver.findElement(By.id("password")).sendKeys("1234");
      //Register Button
      Thread.sleep(3000);
      driver.findElement(By.xpath("/html/body/div/div/div/form/div[5]/button")).click();
      //Login Here Button
      driver.findElement(By.xpath("/html/body/div/div/div/form/div[5]/span/a")).click();
      //Login Details
-     driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("Smith@tcs.com");
-     driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("1999");
+     driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("Tom@tcs.com");
+     driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("1234");
      Thread.sleep(3000);
      driver.findElement(By.xpath("//*[@id=\"login-submit\"]")).click();
      Thread.sleep(3000);
      driver.findElement(By.xpath("/html/body/div/a")).click();
      //Add Employee
      Thread.sleep(3000);
-     driver.findElement(By.xpath("//*[@id=\"firstName\"]")).sendKeys("Jennifer");
-     driver.findElement(By.xpath("//*[@id=\"lastName\"]")).sendKeys("Jones");
-     driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("Jennifer@tcs.com");
+     driver.findElement(By.xpath("//*[@id=\"firstName\"]")).sendKeys(Phoebe");
+     driver.findElement(By.xpath("//*[@id=\"lastName\"]")).sendKeys("Buffay");
+     driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("Phoebe@tcs.com");
      Thread.sleep(3000);
      driver.findElement(By.xpath("html/body/div/form/button")).click();
      driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li/a")).click();  
